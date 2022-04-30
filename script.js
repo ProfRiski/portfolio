034b1a8ad3f53753ca2e23f9a379471046ce479f
ProfRiski <99380737+ProfRiski@users.noreply.github.com>
2022-05-01 04:15:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Student"],
+        strings: ["Minecraft Server Developer", "Discord Bot Developer", "Website Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
